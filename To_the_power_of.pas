997f7bc0b21214e850d6ed574800f28a4a64307a
tomasagustinrossi <diiucapri@gmail.com>
2025-08-03 18:16:40 -0300

    Update To_the_power_of.pas

--- To_the_power_of.pas
+++ To_the_power_of.pas
@@ -1,16 +1,29 @@
 //idea: (pseudocode) get value number and to be elevated of keyboard, make a for 2 to n valor = valor*valor; if one the code for will not be activated so its resolve any posible situation
 program To_the_power_of;
 var
-  p,i,number,resultt:integer;
+  p,i,number,resultt:longint;
 begin
   p:=0;
-  while (p<>9999) do begin
-    writeln('Write a number and next "of" write the power number (if you write 9999 the program ends)');
+  writeln('First, input a number. The program will then display "number to the power of ". After that, input the exponent. To stop the program, enter 999999 when prompted for the exponent.');
+  readln(number);
+  writeln(number,' to power of ');readln(p);
+  while (p<>999999) do begin
+    if number = 0 then
+    begin
+        writeln('The result is 0');
+    end
+    else
+    begin
+      for i:=2 to p do
+        number:=number*number;
+    end;
+    writeln('The result is ',number);
+    writeln;
+    writeln('----------------------------------------------------------------------------------------------------------------');
+    writeln('Input a number, then input its exponent. To stop the program, enter 999999.');
+    writeln('----------------------------------------------------------------------------------------------------------------');
     writeln('Number to Power:');
     readln(number);
     writeln(number,' to power of ');readln(p);
-    for i:=2 to p do
-        number:=number*number;
-    writeln('The result is ',number);
-    end;
+  end;
 end.
